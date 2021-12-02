3725e6d8609102514a5272c1dc8f985a54a3e05c
ShubhamSingh9939 <shubhu9939@gmail.com>
2021-12-02 22:19:58 +0530

    added Diff.java

---
+++ src/com/basic/Test3.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		 int a = 10;
+		 int b= 20;
+		 int result =a-b;
+		 
+		 System.out.println("Sum of Digit"+" "+result);
+	}
+}  
+
+
