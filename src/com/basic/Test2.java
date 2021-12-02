f4e0c701b707566648f6c499ea5d6c40b4096436
ShubhamSingh9939 <shubhu9939@gmail.com>
2021-12-02 22:09:49 +0530

    Test2

---
+++ src/com/basic/Test2.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		 int a = 10;
+		 int b =20;
+		 int result = a*b;
+		 
+		 System.out.println("Multiplication of digits"+" "+result);
+	}
+
+}
